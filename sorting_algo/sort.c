b3c3f6229652fcce4af41ee530162640f56833de
El Mehdi Belfkih <ebelfkih@e1r3p4.1337.ma>
2023-05-08 06:18:56 +0100

    update

--- sorting_algo/sort.c
+++ sorting_algo/sort.c
@@ -6,7 +6,7 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 06:14:22 by ebelfkih          #+#    #+#             */
-/*   Updated: 2023/05/07 06:07:35 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2023/05/08 05:06:02 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,27 +72,24 @@ void	small_sort_5(t_list **stack_a, t_list **stack_b, t_vars *m)
 	{
 		if (tmp->final_pos == 5)
 		{
-			if (tmp->pos == 2)
-				operation(stack_a, stack_b, m, "sa");
-			else if (tmp->pos == 3)
+			if (tmp->pos == 3 || tmp->pos == 2)
 			{
-				operation(stack_a, stack_b, m, "ra");
+				if (tmp->pos == 3)
+					operation(stack_a, stack_b, m, "ra");
 				operation(stack_a, stack_b, m, "sa");
 			}
-			else if (tmp->pos == 4)
+			else if (tmp->pos == 4 || tmp->pos == 5)
 			{
 				operation(stack_a, stack_b, m, "rra");
-				operation(stack_a, stack_b, m, "rra");
+				if (tmp->pos == 5)
+					operation(stack_a, stack_b, m, "rra");
 			}
-			else if (tmp->pos == 5)
-				operation(stack_a, stack_b, m, "rra");
 		}
 		tmp = tmp->next;
 	}
 	operation(stack_a, stack_b, m, "pb");
-	small_sort_4(stack_a, stack_b, m);
-	return (operation(stack_a, stack_b, m, "pa"),
-		operation(stack_a, stack_b, m, "ra"));
+	return (small_sort_4(stack_a, stack_b, m), operation(stack_a,
+			stack_b, m, "pa"), operation(stack_a, stack_b, m, "ra"));
 }
 
 void	sort(t_list **stack_a, t_list **stack_b, t_vars *m)
